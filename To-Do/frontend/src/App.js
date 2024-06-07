c35bbe39219296fe1c572a01dbfe06e1b7ff567c
Sarangi11 <srnginimesha@gmail.com>
2024-06-07 18:56:05 +0530

    add

--- To-Do/frontend/src/App.js
+++ To-Do/frontend/src/App.js
@@ -1,6 +1,6 @@
 
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"; 
-import AddSchedule from './pages/AddSchedule/AddSchedule';
+import Home from './pages/HomePage/Home';
 import DisplayRequest from './pages/DisplaySchedule/DisplaySchedule';
 import UpdateRequest from './pages/UpdateSchedule/UpdateSchedule';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
     <Routes>
-      <Route path='/addSchedule' element={<AddSchedule/>}/>
+      <Route path='' element={<Home/>}/>
       <Route path='/scheduledisplay' element={<DisplayRequest/>}/>
       <Route path='/update/:id' element={<UpdateRequest/>}/>
     </Routes>
