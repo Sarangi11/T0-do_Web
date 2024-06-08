6bf8cc7cb676ae73f5fa51c51d110a187835a43a
Sarangi11 <srnginimesha@gmail.com>
2024-06-08 21:07:19 +0530

    add

--- To-Do/frontend/src/pages/AddTask/AddTask.jsx
+++ To-Do/frontend/src/pages/AddTask/AddTask.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom'; 
+import new1 from "./new1.png";
+import new2 from "./new2.png";
 
 function page() {
   return (
@@ -11,6 +13,11 @@ function page() {
       <Link to="/tasks" style={styles.link}>
         <button style={styles.button} id="button22">Normal Plan</button>
       </Link>
+
+      <div>
+      <img src={new1} alt=""  style={styles.Image}/>
+      <img src={new2} alt="" style={styles.Image} />
+      </div>
     </div>
   );
 }
@@ -43,6 +50,10 @@ const styles = {
     height: '100px',
     backgroundImage: "url('https://images.unsplash.com/photo-1516557070061-c3d1653fa646?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')", 
     backgroundSize: 'cover', 
+  },
+  Image:{
+    width: '90%',
+    maxHeight: '600px', 
   },
 };
 
